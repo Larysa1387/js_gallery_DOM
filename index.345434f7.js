935d801b7253e796fd4d322255347f231dea64fd
larysa1387 <kaliuzhna.off@gmail.com>
2024-12-20 18:25:26 +0200

    make build

---
+++ index.345434f7.js
@@ -0,0 +1,2 @@
+var e=document.querySelector("#largeImg");document.querySelector("#thumbs").addEventListener("click",function(t){t.preventDefault();var r=t.target.closest(".list-item__link");"A"===r.nodeName&&e.setAttribute("src",r.href)});
+//# sourceMappingURL=index.345434f7.js.map
